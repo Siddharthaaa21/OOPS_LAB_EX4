b8b1bdc2969ae7a8a4eb9d235fe38aaf791dfd59
codeelevate321 <500098457@stu.upes.ac.in>
2022-09-20 23:34:40 +0530

    added the lab work

--- decide.java
+++ decide.java
@@ -1,5 +1,72 @@
-//Write a class Worker and derive classes DailyWorker and SalariedWorker from it. Every workerhas a name and a salary rate. Write method ComPay (int hours) to compute the week pay of everyworker. A Daily Worker is paid on the basis of the number of days he/she works. The SalariedWorker gets paid the wage for 40 hours a week no matter what the actual hours are. Test thisprogram to calculate the pay of workers. You are expected to use the concept of polymorphism towrite this program.
+//Write a class Worker and derive classes DailyWorker and SalariedWorker from it. 
+//Every workerhas a name and a salary rate. Write method ComPay (int hours) to compute the week pay of everyworker. 
+//A Daily Worker is paid on the basis of the number of days he/she works. 
+//The SalariedWorker gets paid the wage for 40 hours a week no matter what the actual hours are. 
+//Test thisprogram to calculate the pay of workers.
+// You are expected to use the concept of polymorphism towrite this program.
 
-public class decide {
+public class Worker { 
+     String worker_name;
+    int salory_rate;
+
+    public void Worker(String str,int sal){
+       worker_name=str;
+        salory_rate=sal;
+     
+    }
+    void display()
+    {
+        System.out.println("name is "+worker_name);
+        System.out.println("salory is "+salory_rate);
+
+    }
+}
+    class DailyWorker extends Worker{
+        
+        public void DailyWorker(String name,int salory)
+        {
+            super(name,salory);
+           
+
+        }
+        void display()
+        {
+            System.out.println("name is "+worker_name);
+            System.out.println("salory is "+salory_rate);
+    
+        }
+        public void ComPay(int hours)
+        { display();
+             int rate=salory_rate*hours;
+             System.out.println("The rate/salory is; "+rate);
+        }
+    }
+    class SalariedWorker extends Worker{
+         
+        public void SalariedWorker(String n,int sr)
+        {
+            super(n,sr);
+        }
+        void display()
+        {
+            System.out.println("name is "+worker_name);
+            System.out.println("salory is "+salory_rate);
+    
+        }
+        public void ComPay(int hours)
+        { display();
+             int rate=salory_rate*hours;
+             System.out.println("The rate/salory is; "+rate);
+        }
+
+    }class decide {
+
+    
+    public static void main(String args[])
+    {
+    DailyWorker ob=new DailyWorker("one",8);
+    SalariedWorker obj=new SalariedWorker("two",10);
+
+    }
     
 }
---
+++ employee.java
@@ -0,0 +1,21 @@
+
+//Design a class employee of an organization. An employee has a name, empid, and salary. Write the default constructor, //
+//a constructor with parameters (name, empid, and salary) and methods to return name and salary.
+// Also write a method increaseSalary that raises the employee’s salary by a certain user specified percentage.
+// Derive a subclass Manager from employee. Add an instance variable named department to the manager class. Supply a test program that uses theses classes and methods.-\
+public satatic void main(String args[])
+{
+
+}
+class employee{
+    String name;
+    String emailid;
+    int sal;
+    employee(String name,String emailid,int sal){
+        this.name=name;
+        this.emailid=emailid;
+        this.sal=sal;
+
+
+    }
+}
